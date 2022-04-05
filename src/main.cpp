b047b3b07b157e6d202e37a54eb00783e92772c5
Henrique Utzig <henriqueutzig@gmail.com>
2022-04-05 10:01:49 -0300

    init: trie tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <hashTable.h>
+#include <trieTree.h>
 #include <parser.hpp>
 
 int main(int argc, char const *argv[])
 {
-    HashTable hashTable(10000);
+//    HashTable hashTable(10000);
 
-    std::ifstream f("../data/minirating.csv");
-    aria::csv::CsvParser parser(f);
+  //  std::ifstream f("../data/minirating.csv");
+   // aria::csv::CsvParser parser(f);
 
-    for (auto &row : parser)
-    {
-        hashTable.insert({row[0], row[1]});
-    }
+    //for (auto &row : parser)
+   // {
+     //   hashTable.insert({row[0], row[1]});
+   // }
 
-    std::cout << hashTable.getSize();
+//    std::cout << hashTable.getSize();
+	std::cout << "Hello " << std::endl;
     return 0;
 }
