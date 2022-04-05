b305ee5386855d57690fea83c9bd54d7668d6dc8
Henrique Utzig <henriqueutzig@gmail.com>
2022-04-05 10:32:14 -0300

    feat: trie tree implementation

--- src/TrieTree/trieTree.cpp
+++ src/TrieTree/trieTree.cpp
@@ -1,1 +1,64 @@
 #include "trieTree.h"
+
+TrieTree::TrieTree(void)
+{
+    struct TrieNode *pNode = new TrieNode;
+
+    pNode->isEndOfWord = false;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = NULL;
+
+    this->root = pNode;
+}
+
+TrieTree::~TrieTree()
+{
+}
+
+void TrieTree::insert(string key, int fifaID)
+{
+    struct TrieNode *pCrawl = this->root;
+
+    for (int i = 0; i < key.length(); i++)
+    {
+        int index = key[i] - 'a';
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = this->getNode();
+
+        pCrawl = pCrawl->children[index];
+    }
+
+    pCrawl->isEndOfWord = true;
+    pCrawl->fifaID = fifaID;
+    return;
+}
+
+TrieNode *TrieTree::getNode(void)
+{
+    struct TrieNode *pNode = new TrieNode;
+
+    pNode->isEndOfWord = false;
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = NULL;
+
+    return pNode;
+}
+
+
+int TrieTree::search(string key)
+{
+    struct TrieNode *pCrawl = this->root;
+ 
+    for (int i = 0; i < key.length(); i++)
+    {
+        int index = key[i] - 'a';
+        if (!pCrawl->children[index])
+            return false;
+ 
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    return (pCrawl != NULL && pCrawl->isEndOfWord) ? pCrawl->fifaID: -1;
+}
--- src/TrieTree/trieTree.h
+++ src/TrieTree/trieTree.h
@@ -1,5 +1,30 @@
 #pragma once
 
-#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 #include <string>
 
+#define ALPHABET_SIZE 26
+
+using namespace std;
+
+struct TrieNode
+{
+    struct TrieNode *children[ALPHABET_SIZE];
+    int fifaID;
+    bool isEndOfWord;
+} typedef TrieNode;
+
+class TrieTree
+{
+private:
+    TrieNode *root;
+    TrieNode *getNode(void);
+
+public:
+    TrieTree(void);
+    ~TrieTree();
+    void insert(string key, int fifaID);
+    int search(string key);
+};
